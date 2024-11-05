eebc7c3a25a98cd1da13e95c6a345e47c2503c8c
Sua Chae <ssua918@naver.com>
2024-11-05 23:22:43 +0900

    Set2a Ex9

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -139,7 +139,9 @@ greet first last =
 --   safeIndex ["a","b","c"] (-1)  ==> Nothing
 
 safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i = todo
+safeIndex xs i
+  | i < 0 || i >= length xs = Nothing
+  | otherwise = Just (xs !! i)
 
 ------------------------------------------------------------------------------
 -- Ex 10: another variant of safe division. This time you should use
