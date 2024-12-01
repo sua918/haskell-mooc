810ac72551eff6b2a7efd66aa4e7bb4519ed5149
Sua Chae <ssua918@naver.com>
2024-12-01 23:38:08 +0900

    Set3a Ex6

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -118,7 +118,7 @@ capitalizeFirst (x:xs) = toUpper x : xs
 --   * the function takeWhile
 
 powers :: Int -> Int -> [Int]
-powers k max = todo
+powers k max = takeWhile (<= max) [k^i | i <- [0..max]]
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a functional while loop. While should be a function
